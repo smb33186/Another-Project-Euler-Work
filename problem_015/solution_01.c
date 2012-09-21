f542a5d09dcaa541eedeb23bb4e2af68d703f3c6
Scott B <smb33186@gmail.com>
2012-09-20 18:32:41 -0600

    Initial creation of problem #15 files.

---
+++ problem_015/solution_01.c
@@ -0,0 +1,26 @@
+/*
+ * Solution to Project Euler Problem #15
+ *
+ * Scott Busch
+ *
+ * Modification Log:
+ *	Date			Change
+ *	2012/xx/xx		Initial authoring. 
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int problem(int argc, char** argv) {
+
+	return( 0 );
+}
+
+int main(int argc, char** argv) {
+
+	return( problem(argc, argv) );
+}
