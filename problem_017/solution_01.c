4a4d6bb9fd16431b6990585cbf084872922410d0
sb <smb33186@gmail.com>
2012-12-23 15:47:02 -0700

    Initial solution code file.

---
+++ problem_017/solution_01.c
@@ -0,0 +1,26 @@
+/*
+ * Solution to Project Euler Problem #17
+ *
+ * Scott Busch
+ *
+ * Modification Log:
+ *	Date			Change
+ *	2012/12/23		Initial authoring. 
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int problem(int argc, char** argv) {
+
+	return(0);
+}
+
+int main(int argc, char** argv) {
+
+	return(problem(argc, argv));
+}
